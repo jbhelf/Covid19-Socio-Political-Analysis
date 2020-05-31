97d7257c9e1d0b56584909349810d85b6da1d93c
jbhelf <54462090+jbhelf@users.noreply.github.com>
2020-05-31 16:44:51 -0400

    Created shell file
    File will update/download source covid-19 data

---
+++ Covid-19 Analysis/updateFiles.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#Global confirmed link
+curl 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv' > CovidData/time_series_covid19_confirmed_global.csv
+
+#US confirmed link
+curl 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv' > CovidData/time_series_covid19_confirmed_US.csv
+
+#US deaths link
+curl 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv' > CovidData/time_series_covid19_deaths_US.csv
+
+#Global death link
+curl 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv' > CovidData/time_series_covid19_deaths_global.csv
+
+#Global recovery link
+curl 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv' > CovidData/time_series_covid19_recovered_global.csv
