e55c30842e55789439438089c7a770d29b9737b9
jbhelf <54462090+jbhelf@users.noreply.github.com>
2020-06-01 13:53:50 -0400

    Updated file addresses from absolute to relative

--- Covid-19 Analysis/main.py
+++ Covid-19 Analysis/main.py
@@ -6,23 +6,23 @@ from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    dataLoc = "C:/Users/justi/Desktop/Corona Proj/CovidData/" #Data folder address
+    dataLoc = "CovidData/" #Data folder address
     
-    usConf = dataLoc + "time_series_covid19_confirmed_US.csv" #Path to US confirmed cases data
-    usDeath = dataLoc + "time_series_covid19_deaths_US.csv" #Path to US deaths data
+    usConf = "CovidData/time_series_covid19_confirmed_US.csv" #Path to US confirmed cases data
+    usDeath = "CovidData/time_series_covid19_deaths_US.csv" #Path to US deaths data
 
     """Question 1: Determine the Steady-state solution for country"""
 
     usCEQ = modelData(openUS(usConf))
 
     """World data locations"""
-    wConf = dataLoc + "time_series_covid19_confirmed_global.csv" #Path to world confirmed cases data    
-    wDeath = dataLoc + "time_series_covid19_deaths_global.csv" #Path to world deaths data
-    wRec = dataLoc + "time_series_covid19_recovered_global.csv" #Path to world recovery data
+    wConf = "CovidData/time_series_covid19_confirmed_global.csv" #Path to world confirmed cases data    
+    wDeath = "CovidData/time_series_covid19_deaths_global.csv" #Path to world deaths data
+    wRec = "CovidData/time_series_covid19_recovered_global.csv" #Path to world recovery data
     
     """Socio-Political metrics"""
-    pop = dataLoc + "WPP2019_TotalPopulationBySex.csv" #Path to world population data
-    hdi = dataLoc + "Human development index (HDI).csv" #Path to HDI data
+    pop = "CovidData/WPP2019_TotalPopulationBySex.csv" #Path to world population data
+    hdi = "CovidData/Human development index (HDI).csv" #Path to HDI data
 
     cDat = {}
     cDat["United States of America"] = usCEQ
