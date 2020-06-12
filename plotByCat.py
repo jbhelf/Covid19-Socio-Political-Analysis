bd7774a261ecde663ae6ca71cd5044bcb0981724
Justin Helfman <jhelfman@purdue.edu>
2020-06-12 15:48:01 -0400

    Death rate trend line & distance from origin

--- plotByCat.py
+++ plotByCat.py
@@ -1,19 +1,17 @@
 import matplotlib.pyplot as plt
+import math
 
-def detDist(dct):
-    hDist = {'L':0, 'M':0, 'H':0, 'N':0} #cumulative hdi distances
-    pDist = {'L':0, 'M':0, 'H':0, 'N':0} #cumulative pfi distances
-    eDist = {'L':0, 'M':0, 'H':0, 'N':0} #cumulative education distances
+def detDist(l, m, h):
+    dist = {'L':0, 'M':0, 'H':0, 'N':0} #cumulative distances
 
-    for i in dct:
-        d = (dct[i][0] ** 2 + dct[i][0] ** 2) ** (1/2) #distance
-        hDist[dct[i][2]] += d
-        pDist[dct[i][3]] += d
-        eDist[dct[i][4]] += d
-    
-    print(hDist)
-    print(pDist)
-    print(eDist)
+    for i in l:
+        dist['L'] += math.sqrt(l[i][0] ** 2 + l[i][1] ** 2)
+    for i in m:
+        dist['M'] += math.sqrt(m[i][0] ** 2 + m[i][1] ** 2)
+    for i in h:
+        dist['H'] += math.sqrt(h[i][0] ** 2 + h[i][1] ** 2)
+
+    print(dist)  
         
 
 def plotMetrics(dct):
@@ -25,17 +23,24 @@ def plotMetrics(dct):
             if dct[i][j] == 'H':
                 high[i] = [dct[i][0], dct[i][1]]
             elif dct[i][j] == 'M':
-                 mid[i] = [dct[i][0], dct[i][1]]
+                mid[i] = [dct[i][0], dct[i][1]]
             elif dct[i][j] == 'L':
-                 low[i] = [dct[i][0], dct[i][1]]
+                low[i] = [dct[i][0], dct[i][1]]
 
-       
+        mxC = 0 #recording maximum confirmed percentage
+
         for i in high:
             g = plt.scatter(high[i][0], high[i][1], color='green', label='Upper Tertile')
+            if mxC < high[i][0]:
+                mxC = high[i][0]
         for i in mid:
             o = plt.scatter(mid[i][0], mid[i][1], color='orange', label='Middle Tertile')
+            if mxC < mid[i][0]:
+                mxC = mid[i][0]
         for i in low:
             r = plt.scatter(low[i][0], low[i][1], color='red', label='Lower Tertile')
+            if mxC < low[i][0]:
+                mxC = low[i][0]
         plt.xlabel("Confirmed Cases (Scaled by Population)")
         plt.ylabel("Deaths (Scaled by Population)")
         if j == 2:
@@ -45,5 +50,10 @@ def plotMetrics(dct):
         else:
             plt.title("Confirmed Cases vs. Deaths (Education)")
         plt.legend(handles=[g,o,r])
+        mxC *= 10
+        mxC = math.ceil(mxC + 1) #rounding
+        dr = 408000/7150000 #6/11/20 rough death rate percentage estimate (total deaths/total infected)
+        plt.plot([i/10 for i in range(0,mxC,1)], [i*dr/10 for i in range(0,mxC,1)], color='blue')
         plt.show()
+        detDist(low, mid, high)
          
